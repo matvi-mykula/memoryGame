9511643437c8cfb844a7a41d13858e7bb098ac73
matvimykula <matt.pronchick@gmail.com>
2022-09-30 12:02:31 -0700

    still working

--- src/Components/tiles.js
+++ src/Components/tiles.js
@@ -64,7 +64,7 @@ function shuffle(oldArray) {
     randomIndex;
 
   // While there remain elements to shuffle.
-  while (currentIndex != 0) {
+  while (currentIndex !== 0) {
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
